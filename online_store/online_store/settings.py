8d5b5e4e9782da8bc486a7d6586ee575c9aaeac1
Vatsek Pavel <pvatsek@gmail.com>
2024-03-02 13:28:36 +0300

    fix

--- online_store/online_store/settings.py
+++ online_store/online_store/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False if os.getenv('DEBUG') == 'False'else True
 SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = False
 
 ALLOWED_HOSTS = [
     '192.168.1.34',
